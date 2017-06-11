99d79096807b3cef68037d554676f5c9a9513339
Adam Linkous <alinkous@gmail.com>
2017-06-10 23:45:33 -0500

    add tooling
    - add rake file
    - add rubocop yml file
    - update kitchen file
    - update metabadata file
    - update readme
    
    Signed-off-by: Adam Linkous <alinkous@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,13 @@
 name 'manage_nfs'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Adam Linkous'
+maintainer_email 'alinkous+support@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures manage_nfs'
 long_description 'Installs/Configures manage_nfs'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+supports 'centos'
+chef_version '>= 12.19' if respond_to?(:chef_version)
+issues_url 'https://github.com/gryte/manage_nfs/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/gryte/manage_nfs' if respond_to?(:source_url)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/manage_nfs/issues'
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/manage_nfs'
+depends 'firewall', '~> 2.6.1'
