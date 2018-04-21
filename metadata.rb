c17ab24ef6f41995a6f2df2e2e7e802ac99b6d0c
Adam Linkous <alinkous@gmail.com>
2018-04-20 19:29:41 -0500

    update metadata.rb chef version constraint
    - change to optimistically support 12.19 and up
    - bump patch version
      - bug fix

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures manage_nfs'
 long_description 'Installs/Configures manage_nfs'
-version '0.1.1'
+version '0.1.2'
 supports 'centos'
-chef_version '>= 14.0' if respond_to?(:chef_version)
+chef_version '>= 12.19' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/manage_nfs/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/gryte/manage_nfs' if respond_to?(:source_url)
 
