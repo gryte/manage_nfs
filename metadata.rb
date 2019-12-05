9dd56e7de4c45018f21d113d7dfabd2e26f94d90
Adam Linkous <alinkous@gmail.com>
2019-12-05 17:45:51 -0600

    Bump Firewall Dependency

--- metadata.rb
+++ metadata.rb
@@ -10,5 +10,5 @@ chef_version '>= 12.19' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/manage_nfs/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/gryte/manage_nfs' if respond_to?(:source_url)
 
-depends 'firewall', '~> 2.6.5'
+depends 'firewall', '~> 2.7.0'
 depends 'chef-vault', '~> 3.1.1'
